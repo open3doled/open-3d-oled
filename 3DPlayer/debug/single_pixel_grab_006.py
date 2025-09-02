d5dba474f1a9a4b14f9820c6f172996599330dc6
open3doled <159269811+open3doled@users.noreply.github.com>
2025-09-02 13:20:23 +0900

    more debug changes and package install logs

--- 3DPlayer/debug/single_pixel_grab_006.py
+++ 3DPlayer/debug/single_pixel_grab_006.py
@@ -3,6 +3,7 @@ import glfw
 from OpenGL.GL import *
 
 # pacman -S mingw-w64-ucrt-x86_64-glfw
+# pip install glfw
 
 USE_LINE_PROFILER = True
 if USE_LINE_PROFILER:
@@ -16,6 +17,7 @@ else:
             return func(*args, **kwargs)
 
         return wrapper
+
 
 user32 = ctypes.windll.user32
 gdi32 = ctypes.windll.gdi32
@@ -41,24 +43,25 @@ glfw.swap_interval(1)
 
 
 # Give the system a moment to warm up
-time.sleep(0)
+time.sleep(5)
 
 colors = {}
 start = time.time()
 
+
 @line_profiler_obj
 def loop():
     # Main loop
-    #while not glfw.window_should_close(window):
-    for _ in range(1200):
+    # while not glfw.window_should_close(window):
+    for _ in range(2400):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         # Render your OpenGL content here
-    
+
         glfw.swap_buffers(window)
-        
+
         # Grab the latest frame from the desktop
         data = gdi32.GetPixel(hdc, 0, 0)
-    
+
         if data != -1:
             r = data & 0xFF
             g = (data >> 8) & 0xFF
@@ -66,12 +69,13 @@ def loop():
             color = (r, g, b)
         else:
             color = None
-        
+
         # Count occurrences
         colors[color] = colors.get(color, 0) + 1
-        
+
         glfw.poll_events()
-        
+
+
 loop()
 
 elapsed = time.time() - start
